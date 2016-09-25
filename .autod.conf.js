6dcde03561cc580d00e818d662c7ff222285cfa2
庞锦贵 <jingui.pjg@alibaba-inc.com>
2016-09-26 02:58:56 +0800

    feat: first commit

---
+++ .autod.conf.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  write: true,
+  prefix: '~',
+  devprefix: '^',
+  exclude: [
+    'test/fixtures'
+  ],
+  dep: [
+  devdep: [
+    'autod',
+    'eslint',
+  ],
+  keep: [
+  ],
+  semver: [
+  ]
+};
---
+++ vbd-conf.js
@@ -0,0 +1,4 @@
+const vbd = require('./node_modules/vbd');
+vbd.match('vbd-cfg.js', {
+  release: false
+});
