60d7a8c9ba953d9a1bd8b3f7a0114a4ee649d0c8
庞锦贵 <jingui.pjg@alibaba-inc.com>
2016-10-13 23:00:35 +0800

    feat: start demo

--- common/cookie.js
+++ common/cookie.js
@@ -1,38 +1,36 @@
-(function (global) {
-    var cookie = {
-        set: function (key, val, time) {
-            var date = new Date(),
-                expiresDays = time,
-                url = location.href,
-                cookieStr;
+const cookie = {
+  set(key, val, time) {
+    const date = new Date();
+    const expiresDays = time;
+    const url = location.href;
+    let cookieStr;
 
-            date.setTime(date.getTime() + expiresDays * 24 * 3600 * 1000);
+    date.setTime(date.getTime() + (expiresDays * 24 * 3600 * 1000));
 
-            if (url.indexOf('cricuc.com') > -1) {
-                cookieStr = ';domain=.cricuc.com;path=/;';
-            } else if (url.indexOf('ucweb.com') > -1) {
-                cookieStr = ';domain=.ucweb.com;path=/;';
-            } else if (url.indexOf('uc.cn') > -1) {
-                cookieStr = ';domain=.uc.cn;path=/;';
-            }
-
-            document.cookie = key + '=' + val + ';expires=' + date.toGMTString() + cookieStr;
-
-        },
-        get: function (key) {
-            var cookie = document.cookie.replace(/[ ]/g, ''),
-                arrCookie = cookie.split(';'),
-                tips;
-
-            for (var i = 0; i < arrCookie.length; i++) {
-                var arr = arrCookie[i].split('=');
-                if (key === arr[0]) {
-                    tips = arr[1];
-                    break;
-                }
-            }
-            return tips;
-        }
+    if (url.indexOf('cricuc.com') > -1) {
+      cookieStr = ';domain=.cricuc.com;path=/;';
+    } else if (url.indexOf('ucweb.com') > -1) {
+      cookieStr = ';domain=.ucweb.com;path=/;';
+    } else if (url.indexOf('uc.cn') > -1) {
+      cookieStr = ';domain=.uc.cn;path=/;';
     }
-    global._cookie = cookie;
-})(window);
+
+    document.cookie = `${key}=${val};expires=${date.toGMTString()}${cookieStr}`;
+  },
+  get(key) {
+    const _cookie = document.cookie.replace(/[ ]/g, '');
+    const arrCookie = _cookie.split(';');
+    let tips;
+
+    for (let i = 0; i < arrCookie.length; i += 1) {
+      const arr = arrCookie[i].split('=');
+      if (key === arr[0]) {
+        tips = arr[1];
+        break;
+      }
+    }
+    return tips;
+  }
+};
+
+module.exports = cookie;
--- views/pages/list.js
+++ views/pages/list.js
@@ -3,10 +3,10 @@ const list = require('components/list');
 const nav = require('components/nav');
 const cookie = require('common/cookie');
 
-console.log(cookie); // eslint-disable-line
-console.log('这是page-index'); // eslint-disable-line
+console.log('这是page-list'); // eslint-disable-line
 const uid = cookie.get('uid');
 if (!uid) {
+  alert(1);
   header.init();
   list.init();
   nav.init();
